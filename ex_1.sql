eb98e2a2a02963ca471e3fd83098418acedc28d8
atveyaru <89899116+atveyaru@users.noreply.github.com>
2022-12-15 15:27:20 +0400

    Update ex_1.sql

--- ex_1.sql
+++ ex_1.sql
@@ -1,7 +1,7 @@
 
 drop table IF exists ordersitem;
-drop table IF exists products;
 drop table IF exists orders;
+drop table IF exists products;
 
 CREATE TABLE products (
 id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'уникальное значение',
@@ -17,8 +17,9 @@ name varchar(10) NOT NULL COMMENT 'Комментарий'
 CREATE TABLE ordersitem (
 id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'уникальное значение',
 id_orders int(11) NOT NULL COMMENT 'ссылка на Заказы',
-id_products int(11) NOT NULL COMMENT 'ссылка на Товары'
-) COMMENT = 'Заказы';
+id_products int(11) NOT NULL COMMENT 'ссылка на Товары', 
+qty int(10) NOT NULL COMMENT 'Кол-во товара в заказе' 
+) COMMENT = 'Позиции заказов';
 
 ALTER TABLE ordersitem ADD CONSTRAINT ordersitem_fk1 
 						   FOREIGN KEY (id_products) REFERENCES products(id);
@@ -36,24 +37,18 @@ INSERT INTO orders(date_orders, name)
 VALUES 
 (str_to_date('01.04.2021', '%d.%m.%Y'), 'Какой-то камментарий');
 
-INSERT INTO ordersitem(id_orders, id_products)
+INSERT INTO ordersitem(id_orders, id_products, qty)
 VALUES 
-(1, 1),
-(1, 2),
-(1, 2),
-(1, 3),
-(1, 3),
-(1, 3),
-(1, 4);
+(1, 1, 2),
+(1, 2, 2),
+(1, 3, 1),
+(1, 4, 3);
 
 COMMIT;
 
-SELECT p.name, COUNT(oi.id)
+SELECT p.name, COUNT(oi.id), oi.qty 
 FROM products p, orders o, ordersitem oi
 WHERE p.id = oi.id_products 
   AND oi.id_orders = o.id
-GROUP BY p.name;
-
-COMMIT;
-
+GROUP BY p.name, oi.qty ;
 
